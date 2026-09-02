912aed344273b866734efdb38bfdb9d2c82f608e
scfl1 <huh630841@gmail.com>
2026-09-02 14:46:30 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app" :dir="dir">
+    <router-view />
+    <BottomNav v-if="showNav" />
+  </div>
+</template>
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import BottomNav from './components/BottomNav.vue'
+const route = useRoute()
+const showNav = computed(() => route.meta.bottomNav === true)
+const dir = computed(() => localStorage.getItem('cwaax_lang') === 'en' ? 'ltr' : 'rtl')
+</script>
