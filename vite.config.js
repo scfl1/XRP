8001527e0c57475c290528e5c390496dc858bcb2
scfl1 <huh630841@gmail.com>
2026-09-02 15:15:55 +0300

    Remove Cloudflare plugin from Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    cloudflare()
+    vue()
   ],
 
   resolve: {
