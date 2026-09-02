12c0641dad72ec562cbdc1e5e86885d34e904858
scfl1 <huh630841@gmail.com>
2026-09-02 13:26:23 +0300

    Add Cloudflare plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    cloudflare()
+  ],
 
   resolve: {
     alias: {
