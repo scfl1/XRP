bdeef029bb2330af666c4df93c6838be29303768
scfl1 <huh630841@gmail.com>
2026-09-02 14:51:42 +0300

    Add files via upload

---
+++ src/components/CwaLogo.vue
@@ -0,0 +1,2 @@
+<template><div class="logo"><span>C</span><b>wa</b><span>AX</span></div></template>
+<style scoped>.logo{width:58px;height:58px;border-radius:50%;background:#17191d;color:#fff;display:grid;place-items:center;font-weight:800;font-size:15px;letter-spacing:-1px;border:2px solid #bdc987;box-shadow:inset 0 0 0 2px #17191d}.logo span:last-child{color:#91b927}.logo b{color:#eee;font-weight:800}</style>
---
+++ src/components/Quick.vue
@@ -0,0 +1,1 @@
+<template><button><i :class="icon"></i><span>{{text}}</span></button></template><script setup>defineProps({icon:String,text:String})</script>
