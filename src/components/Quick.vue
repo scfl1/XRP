7d0add9e94d09ac7ed36275758df75abd510fd4c
scfl1 <huh630841@gmail.com>
2026-09-02 16:14:49 +0300

    Delete src directory

--- src/components/Quick.vue
+++
@@ -1,1 +0,0 @@
-<template><button><i :class="icon"></i><span>{{text}}</span></button></template><script setup>defineProps({icon:String,text:String})</script>
--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-import {initializeApp} from 'firebase/app'
-import {getAuth} from 'firebase/auth'
-import {getFirestore} from 'firebase/firestore'
-const firebaseConfig={apiKey:'AIzaSyDa0XdGA05G3vt-enGPBfDTD16K4OdoMik',authDomain:'american-54cbd.firebaseapp.com',projectId:'american-54cbd',storageBucket:'american-54cbd.appspot.com',messagingSenderId:'166955679884',appId:'1:166955679884:web:5d8701f7ed3e3f78d33ba9'}
-const app=initializeApp(firebaseConfig)
-export const auth=getAuth(app)
-export const db=getFirestore(app)
-export default app
